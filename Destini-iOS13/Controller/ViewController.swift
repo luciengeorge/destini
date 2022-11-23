13cbe28e237701e6516dbfa6d8932406115a74c1
Lucien George <lucien.george@shopify.com>
2022-11-23 17:07:45 -0500

    finished destini app

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyRepository: StoryRepository = StoryRepository()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupStory()
     }
 
-
+    func setupStory() {
+        let story = storyRepository.currentStory()
+        storyLabel.text = story.title
+        choice1Button.setTitle(story.choice1, for: .normal)
+        choice2Button.setTitle(story.choice2, for: .normal)
+    }
+    
+    @IBAction func choiceClicked(_ sender: UIButton) {
+        let story = storyRepository.currentStory()
+        let selectedChoice: String = sender.currentTitle!
+        if selectedChoice == story.choice1 {
+            storyRepository.setStoryNumber(destination: story.choice1Destination)
+        } else {
+            storyRepository.setStoryNumber(destination: story.choice2Destination)
+        }
+        setupStory()
+    }
 }
 
